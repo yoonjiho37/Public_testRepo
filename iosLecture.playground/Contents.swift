d1a355343298d0fbb42b45dbe25ee5a9c4dadc0c
dev-yeon <110480577+dev-yeon@users.noreply.github.com>
2023-04-23 11:23:04 +0900

    ver1

---
+++ iosLecture.playground/Contents.swift
@@ -0,0 +1,35 @@
+//import Foundation //얘는 뭘 까?
+import SwiftUI
+
+// 여기는 프로젝트가 아니고, 그냥 문법 연습하거나 그런 곳
+// 위에서 -> 아래로 코드를 실행함
+
+//MARK: -let vs var
+
+let name : String = "유재호"
+var age : Int =22
+print(age)
+
+let link : URL(String:"naver.com")!
+
+// MARK : -primitive types
+//String , Int, Double, CGFloat
+
+// MARK :  struck vs class
+// 일단 " 사용자 정의 타입". class / struct / enum
+// 값 타입. vs 참조타입
+// enum ,struct -> 값 타입
+// class -> 참조 타입
+// 기본적으로 Swift 언어는 값 타입을 좋아함 . light - weight
+// 옵셔널을 안전하게 벗길수 있는 기술
+
+
+
+// MARK :  - 문자열 보관법
+
+
+// MARK :  - 옵셔널 바인딩
+
+
+
+var greeting = "Hello, playground"
